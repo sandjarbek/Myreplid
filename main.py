738f1cbc637b5405556940aa0b13d5c8e2aa2971
sandjarbek <121221063+sandjarbek@users.noreply.github.com>
2023-02-03 06:29:14 +0000

    popup chiqib qolishini oldini oladi

--- main.py
+++ main.py
@@ -1,4 +1,23 @@
-print("sanjar")
-print("hi again")
+from selenium import webdriver
 
-print("by-by")
+
+def get_driver():
+    # Set option to make browsing easer
+    options = webdriver.ChromeOptions()
+    options.add_argument("disable-infobars")
+    options.add_argument("start-maximized")
+    options.add_argument("disable-dev-shm-usage")
+    options.add_argument("no-sandbox")
+    options.add_experimental_option("excludeSwitches", ['enable-automation'])
+    options.add_argument("disable-blink-features=AutomationControlled")
+
+    driver = webdriver.Chrome(options=options)
+    driver.get("https://www.pythonanywhere.com/")
+    return driver
+
+def main():
+    driver=get_driver()
+    element = driver.find_element(by="xpath", value="/html/body/div[1]/div[2]/div/div/h1")
+    return element.text
+
+print(main())
